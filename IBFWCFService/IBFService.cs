d7527d6c64097dbfe15311e8246ae765ecca05da
tamar.bliadze <tamar.bliadze@TAMAR-BLIADZE>
2018-07-20 20:18:33 +0400

    c

--- IBFWCFService/IBFService.cs
+++ IBFWCFService/IBFService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace IBFWCFService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class IBFService : IIBFService
     {
         public string GetData(int value)
         {
--- IBFWCFService/IIBFService.cs
+++ IBFWCFService/IIBFService.cs
@@ -9,7 +9,7 @@ namespace IBFWCFService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IIBFService
     {
         [OperationContract]
         string GetData(int value);
