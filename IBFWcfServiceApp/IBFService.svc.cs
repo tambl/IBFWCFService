8ea375302ec46ad0b39ace142c26d35c62b12008
tamar.bliadze <tamar.bliadze@TAMAR-BLIADZE>
2018-09-03 19:32:41 +0400

    agent + client

--- IBFWcfServiceApp/IBFService.svc.cs
+++ IBFWcfServiceApp/IBFService.svc.cs
@@ -17,7 +17,7 @@ namespace IBFWcfServiceApp
     public class IBFService : IIBFService
     {
 
-        public List<PolicyDto> GetPolicies(string ids, string isConfirmed, string startDate, string endDate, string count, string versionIds, string policyNumber)
+        public List<PolicyDto> GetPolicies(string ids, string isConfirmed, string startDate, string endDate, string count, string versionIds, string policyNumber, string clientIds)
         {
             try
             {
@@ -37,11 +37,21 @@ namespace IBFWcfServiceApp
 
                 var spletVersionIDsConverted = new List<int>();
 
-                if (ids != "null" && !string.IsNullOrEmpty(versionIds))
+                if (versionIds != "null" && !string.IsNullOrEmpty(versionIds))
                 {
-                    foreach (var item in ids.Split(','))
+                    foreach (var item in versionIds.Split(','))
                     {
                         spletVersionIDsConverted.Add(Convert.ToInt32(item));
+                    }
+                }
+
+                var spletClientIDsConverted = new List<int>();
+
+                if (clientIds != "null" && !string.IsNullOrEmpty(clientIds))
+                {
+                    foreach (var item in clientIds.Split(','))
+                    {
+                        spletClientIDsConverted.Add(Convert.ToInt32(item));
                     }
                 }
 
@@ -54,6 +64,7 @@ namespace IBFWcfServiceApp
 
                 using (var dbContext = new IBFEntities())
                 {
+
                     var policiesTemp = (from p in dbContext.Policies
                                         join pv in dbContext.PolicyVersions on p.Id equals pv.PolicyId
                                         join curr in dbContext.Currencies on p.CurrencyId equals curr.Id
@@ -74,14 +85,15 @@ namespace IBFWcfServiceApp
 
                                         where pv.IsHidden == false && pv.IsDelete == false && pv.PolicyStatusId == 3
                                         //&& pv.Id == 4597//3586                                     
-                                        && (startDateConverted != null && endDateConverted != null ? pv.CreateDate > startDateConverted && pv.CreateDate <= endDateConverted :
-                                         startDateConverted != null && endDateConverted == null ? pv.CreateDate > startDateConverted :
-                                         startDateConverted == null && endDateConverted != null ? pv.CreateDate <= endDateConverted :
+                                        && (startDateConverted != null && endDateConverted != null ? pv.StartDate > startDateConverted && pv.StartDate <= endDateConverted :
+                                         startDateConverted != null && endDateConverted == null ? pv.StartDate > startDateConverted :
+                                         startDateConverted == null && endDateConverted != null ? pv.StartDate <= endDateConverted :
                                          1 == 1)
                                         && (isConfirmedConverted == true ? pv.To1CSynchronizeDate != null : pv.To1CSynchronizeDate == null)
                                         && (spletIDsConverted.Count() > 0 ? spletIDsConverted.Contains(p.Id) && pv.IsActive == true : 1 == 1)
                                         && (spletVersionIDsConverted.Count() > 0 ? spletVersionIDsConverted.Contains(pv.Id) : 1 == 1)
                                         && ((policyNumber != "null" && !string.IsNullOrEmpty(policyNumber)) ? p.PolicyNumber == policyNumber : 1 == 1)
+                                        && (spletClientIDsConverted.Count() > 0 ? spletClientIDsConverted.Contains(clientl.Id) : 1 == 1)
                                         select new
                                         {
                                             PolicyId = p.Id,
@@ -90,6 +102,7 @@ namespace IBFWcfServiceApp
                                             PolicyVersionIsActive = pv.IsActive,
                                             StartDate = pv.StartDate,
                                             EndDate = pv.EndDate,
+                                            PolicyCreateDate = p.CreateDate,
                                             PolicyStatusId = pv.PolicyStatusId,
                                             PolicyStatus = pv.PolicyStatu.Name,
                                             PolicyVersionStatusId = pv.PolicyVersionStatusId,
@@ -110,6 +123,7 @@ namespace IBFWcfServiceApp
                                             commands = pv.PolicyPaymentCoverContractCommands.Where(r => r.IsActive == true && r.IsDeleted == false)
                                         }).Take(countConverted != null && countConverted <= 500 ? (int)countConverted : 500).ToList();
 
+                    //var currencyRatesForSpecificDate = dbContext.CurrencyRates.Where(w => w.RateDate == policiesTemp.FirstOrDefault(f => f.PolicyCreateDate));
 
                     policies = policiesTemp.Select(s =>
 
@@ -152,7 +166,7 @@ namespace IBFWcfServiceApp
                             AgentBrokerContractNumber = p.AgentBroker.ContractNo,
                             AgentBrokerContractStartDate = p.AgentBroker.StartDate,
                             AgentBrokerContractEndDate = p.AgentBroker.EndDate,
-                           // AgentBrokerCurrency = p.PolicyPaymentCoverAgentContracts.Select(s=>s.Amount * s.PolicyVersion.Policy.Currency.)
+                            // AgentBrokerCurrency = p.PolicyPaymentCoverAgentContracts.Select(s=>s.Amount * s.PolicyVersion.Policy.Currency.)
                         }).Distinct().ToList()
                     }
                     ).ToList();
