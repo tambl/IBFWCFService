08ead4b921b53fe1caca8c527c4cb421a42dc242
Tamuna Bliadze <tamuna.b813@yahoo.com>
2018-12-07 13:26:22 +0400

    commit with BasciAuth uncommented

--- IBFWcfServiceApp/IIBFService.cs
+++ IBFWcfServiceApp/IIBFService.cs
@@ -22,26 +22,26 @@ namespace IBFWcfServiceApp
         ////http://localhost:8732/IBFService/policies/?id=1&id=2&isConfirmed=true&startDate=01-01-2013&endDate=01-02-2013&count=5
         //List<PersonDto> GetPolicies(string ids, string isConfirmed, string startDate, string endDate, string count);
 
-        [OperationContract]
-        [WebInvoke(Method = "GET", UriTemplate = "policies/?id={ids}&isConfirmed={isConfirmed}&startDate={startDate}&endDate={endDate}&count={count}&versionId={versionIds}&policyNumber={policyNumber}&clientId={clientIds}", ResponseFormat = WebMessageFormat.Json)]
-        //http://localhost:8732/IBFService.svc/policies/?id=1&id=2&isConfirmed=true&startDate=01-01-2013&endDate=01-02-2013&count=5
-        List<PolicyDto> GetPolicies(string ids, string isConfirmed, string startDate, string endDate, string count, string versionIds, string policyNumber, string clientIds);
+        //[OperationContract]
+        //[WebInvoke(Method = "GET", UriTemplate = "policies/?id={ids}&isConfirmed={isConfirmed}&startDate={startDate}&endDate={endDate}&count={count}&versionId={versionIds}&policyNumber={policyNumber}&clientId={clientIds}", ResponseFormat = WebMessageFormat.Json)]
+        ////http://localhost:8732/IBFService.svc/policies/?id=1&id=2&isConfirmed=true&startDate=01-01-2013&endDate=01-02-2013&count=5
+        //List<PolicyDto> GetPolicies(string ids, string isConfirmed, string startDate, string endDate, string count, string versionIds, string policyNumber, string clientIds);
 
-        [OperationContract]
-        [WebInvoke(Method = "PUT", UriTemplate = "policies/?id={ids}", ResponseFormat = WebMessageFormat.Json)]
-        //http://localhost:8732/IBFService.svc/policies/?id=1&id=2
-        //HttpResponseMessage UpdatePolicyVersionSyncDate(string ids);
-        bool UpdatePolicyVersionSyncDate(string ids);
+        //[OperationContract]
+        //[WebInvoke(Method = "PUT", UriTemplate = "policies/?id={ids}", ResponseFormat = WebMessageFormat.Json)]
+        ////http://localhost:8732/IBFService.svc/policies/?id=1&id=2
+        ////HttpResponseMessage UpdatePolicyVersionSyncDate(string ids);
+        //bool UpdatePolicyVersionSyncDate(string ids);
 
-        [OperationContract]
-        [WebInvoke(Method = "GET", UriTemplate = "transfers/?id={ids}&isConfirmed={isConfirmed}&startDate={startDate}&endDate={endDate}&clientId={clientIds}&bankAccounts={bankAccounts}&count={count}", ResponseFormat = WebMessageFormat.Json)]
-        //http://localhost:8732/IBFService.svc/transfers/?id=1&id=2&isConfirmed=true&startDate=01-01-2013&endDate=01-02-2013&count=5
-        List<TransferDto> GetTransfers(string ids, string isConfirmed, string startDate, string endDate, string clientIds, string bankAccounts, string count);
+        //[OperationContract]
+        //[WebInvoke(Method = "GET", UriTemplate = "transfers/?id={ids}&isConfirmed={isConfirmed}&startDate={startDate}&endDate={endDate}&clientId={clientIds}&bankAccounts={bankAccounts}&count={count}", ResponseFormat = WebMessageFormat.Json)]
+        ////http://localhost:8732/IBFService.svc/transfers/?id=1&id=2&isConfirmed=true&startDate=01-01-2013&endDate=01-02-2013&count=5
+        //List<TransferDto> GetTransfers(string ids, string isConfirmed, string startDate, string endDate, string clientIds, string bankAccounts, string count);
 
-        [OperationContract]
-        [WebInvoke(Method = "PUT", UriTemplate = "transfers/?id={ids}", ResponseFormat = WebMessageFormat.Json)]
-        //http://localhost:8732/IBFService.svc/transfers/?id=1&id=2
-        bool UpdateTransfersSyncDate(string ids);
+        //[OperationContract]
+        //[WebInvoke(Method = "PUT", UriTemplate = "transfers/?id={ids}", ResponseFormat = WebMessageFormat.Json)]
+        ////http://localhost:8732/IBFService.svc/transfers/?id=1&id=2
+        //bool UpdateTransfersSyncDate(string ids);
 
         [OperationContract]
         [WebInvoke(Method = "GET", UriTemplate = "personell/", ResponseFormat = WebMessageFormat.Json)]
